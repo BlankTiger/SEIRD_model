Fixed overflow due to dt being too high

utils/drawing.py
@@ -55,7 +55,7 @@ def create_updated_fig_SEIRD(
     coeff = params["-PARAMTAB-"]
     contact = params["-CONTACTTAB-"]
     t, y = seird_math.solve_seird(
-        (0, t_1), y0.astype(np.float64), coeff, contact, params_vac, vac_E, 1
+        (0, t_1), y0.astype(np.float64), coeff, contact, params_vac, vac_E, 0.1
     )
 
     fig = plots.plot_SEIRD(t, y, screen_size)